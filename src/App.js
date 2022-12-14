2da427799fdb56dd8d5d33305c1aa1095bd69352
Gabriel Navarro <alt64b@gmail.com>
2022-12-13 22:44:46 -0300

    agregando img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ImgMeme from './component/imput_text';
 
 function App() {
   return (
     <div className="App">
-     <ImgMeme/>
+    <ImgMeme/>
     </div>
   );
 }
--- src/component/imput_text.js
+++ src/component/imput_text.js
@@ -1,25 +1,37 @@
 import html2canvas from "html2canvas";
+import React, {useState} from "react";
+
+
 const ImgMeme=()=>{
+    //un parametro y una setfuction
+    const [imgMeme, setImgMeme]=useState(1);
+  
+    const selectImg=(e)=>{
+        setImgMeme(e.target.value);
+        console.log(e.target.value);
+
+    }
     return (
      <div className="text-center">
         <h1 className="mt-3 mb-3 text-center">
             Edita tu propio MeMe
         </h1>
         <h3 className="mt-3 mb-3 text-center" >Ingresa texto</h3>
-        <input className="form-control w-50 m-50 m-auti d-block " placeholder="Ingresa Frase" name="meme" aria-label="default input example"></input>
-        <h3 className="" >Tu imagen</h3>
-        <select className="">
+        <input className="form-control w-50 m-50 m-auto d-block " placeholder="Ingresa Frase" name="meme" aria-label="default input example" type="text"></input>
+        <h3 className="mt-3 mb-3 text-center" >Tu imagen</h3>
+        
+        <select onChange={selectImg} className="form-select form-select-lg mb-3 w-50 m-auto"  aria-label=".form-select-lg example">
             <option value={1}>Imagen 1</option>
             <option value={2}>Imagen 2</option>
             <option value={3}>Imagen 3</option>
             <option value={4}>Imagen 4</option>
         </select>
-        <figure className="" id="importimg">
-            <p className=""> </p>
-            <img src={'./images/1.jpg'} className="" alt="meme"/>
+        <figure className="text-center" id="importimg">
+            <p className="w-100 px-30 position-absolute top-50 start-30 h1 text-center"> </p>
+            <img src={'./img/img${imgMeme}.jpg'} className="figure-img img-fluid mt-3 d-block m-auto" alt="meme"/>
         </figure>
-    <button type="button" className="">Descargar </button>
-     
+    <button type="button" className="btn btn-primary mt-4 mb-4">Descargar </button>
+        
      </div>
     )
 }
